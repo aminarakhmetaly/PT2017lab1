ef52d0bc7e52d02ef330b71a85c155e4b8a1551a
Amina <rakhmetslinaamina@gmail.com>
2017-01-30 10:09:34 +0600

    lab

---
+++ lb1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lb1
+{
+    class Complex
+    {
+        int a;
+        int b;
+        public Complex(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        public static Complex operator +(Complex c1, Complex c2)
+        {
+            Complex res = new Complex(0, 0);
+
+            res.a = c1.a + c2.a;
+            res.b = c1.b + c2.b;
+
+            return res;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} + {1}i", a, b);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex c = new Complex(5, 6);
+            Console.WriteLine(c);
+        }
+    }
+}
